ec6b5306497a11eb7870b1cd34015e335f70902b
adrien <adrien.nhem@gmail.com>
2022-04-22 16:29:45 +0200

    update doc

--- app/controllers/auth_controller.rb
+++ app/controllers/auth_controller.rb
@@ -4,14 +4,16 @@ class AuthController < ApplicationController
             space = params[:space]
             payload = { data: 'test' }
 
-            if space == "pe201-design-and-manage"
-                gitbook_sign_key = "f8e9615b-9e2f-4c76-977e-bb144266360a"
-            elsif space == "pe101-deploy-and-discover"
-                gitbook_sign_key = "ca1892d8-6997-4da7-b28e-df8648b5f78e"
+            if space == "squad-acquisition"
+                gitbook_sign_key = "d1ce8f3a-9bee-4757-8913-a00cbddb9360"
+            elsif space == "squad-fleet"
+                gitbook_sign_key = "2e6aa8f2-f6d5-4cdb-98ac-3d9c6b0cd4db"
+            elsif space == "squad-payments"
+                gitbook_sign_key = "b2468b3c-a9f3-409a-aca6-7f57c30fea27"
             end
 
             token = JWT.encode payload, gitbook_sign_key, 'HS256'
-            redirect_to "https://puppet-demo.gitbook.io/#{space}/?jwt_token=#{token}"
+            redirect_to "https://kovi-1.gitbook.io/#{space}/?jwt_token=#{token}"
         else
             redirect_to root_path, notice: "You are not authorized to access this content!"
         end
